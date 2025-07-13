598d90c2ea29af54e02e571fac5894a64502a075
ppparkta <pppppark2@gmail.com>
2025-07-13 22:33:26 +0900

    feat: 수양 2주차 - BOJ1260

--- 수양/2주차/BOJ1260.java
+++ 수양/2주차/BOJ1260.java
@@ -1,39 +1,78 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 // https://www.acmicpc.net/problem/1260
 public class BOJ1260 {
-    public static void main(String[] args) throws IOException {
-        // 첫째 줄에 정점의 개수 N(1 ≤ N ≤ 1,000),
-        // 간선의 개수 M(1 ≤ M ≤ 10,000),
-        // 탐색을 시작할 정점의 번호 V가 주어진다.
-        // 다음 M개의 줄에는 간선이 연결하는 두 정점의 번호가 주어진다.
-        // 어떤 두 정점 사이에 여러 개의 간선이 있을 수 있다.
-        // 입력으로 주어지는 간선은 양방향이다.
+    static int Edge_arr[][];
+    static boolean Visited_arr[];
+    static int N;
+    static int M;
+    static int V;
+    static int count;
+    static Queue<Integer> que = new ArrayDeque<>();
+
+    public static void BFS(int start) {
+        que.offer(start);
+        Visited_arr[start] = true;
+        System.out.print(start + " ");
+
+        while (!que.isEmpty()) {
+            start = que.poll();
+
+            for (int i = 1; i <= N; i++) {
+
+                if (Edge_arr[start][i] == 1 && Visited_arr[i] == false) {
+                    que.offer(i);
+                    Visited_arr[i] = true;
+                    System.out.print(i + " ");
+                }
+            }
+        }
+    }
+
+    public static void DFS(int start) {
+        Visited_arr[start] = true;
+        System.out.print(start + " ");
+
+        if (count == N) {
+            return;
+        }
+        count++;
+
+        for (int i = 1; i <= N; i++) {
+            if (Edge_arr[start][i] == 1 && Visited_arr[i] == false) {
+                DFS(i);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int v = Integer.parseInt(st.nextToken());
+        StringTokenizer st = null;
 
-        boolean visit[] = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            visit[i] = false;
-        }
-        // 노드에 대해 간선을 연결한다 (좌/우 숫자로 연결을 의미한다)
-        Map<Integer, List<Integer>> arr = new HashMap<>();
-        for (int i = 0; i < 5; i++) {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+
+        Edge_arr = new int[1001][1001];
+        Visited_arr = new boolean[1001];
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            Edge_arr[x][y] = Edge_arr[y][x] = 1;
         }
 
+        DFS(V);
+        System.out.println();
 
+        Visited_arr = new boolean[1001];
+        BFS(V);
     }
 }
